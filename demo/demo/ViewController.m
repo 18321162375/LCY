5d3b68161db81254fdb77d449bad13d1e072ff13
LCY <lcy@LCYdeMacBook-Air.local>
2015-10-08 12:37:18 +0800

    1234

---
+++ demo/demo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by LCY on 15/10/8.
+//  Copyright (c) 2015年 LCY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"123");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
